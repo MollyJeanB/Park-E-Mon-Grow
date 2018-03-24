f1c88f4d9befa53c87b702c8592e8aeb0af0207c
Katy LaFrance <snerdette@gmail.com>
2018-03-24 12:18:07 -0700

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import headerContainer from './headerContainer';
+
+export const Header = () => (
+    <header>
+        Just header stuff
+    </header>
+);
+
+export default headerContainer(Header);
