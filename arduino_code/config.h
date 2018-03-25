121f2476a7849ddfced42ce44258224e44766f24
Molly Jean Bennett <mollyjeanbennett@gmail.com>
2018-03-25 13:04:17 -0700

    add arduino code

---
+++ arduino_code/config.h
@@ -0,0 +1,21 @@
+/************************ Adafruit IO Config *******************************/
+
+// visit io.adafruit.com if you need to create an account,
+// or if you need your Adafruit IO key.
+#define IO_USERNAME    "missyt"
+#define IO_KEY         "fdf568b9190c4c01af03667d3d43fa0d"
+
+/******************************* WIFI **************************************/
+
+// the AdafruitIO_WiFi client will work with the following boards:
+//   - HUZZAH ESP8266 Breakout -> https://www.adafruit.com/products/2471
+//   - Feather HUZZAH ESP8266 -> https://www.adafruit.com/products/2821
+//   - Feather M0 WiFi -> https://www.adafruit.com/products/3010
+//   - Feather WICED -> https://www.adafruit.com/products/3056
+
+#define WIFI_SSID       "Puppet Guest"
+#define WIFI_PASS       "argon4949"
+
+// comment out the following two lines if you are using fona or ethernet
+#include "AdafruitIO_WiFi.h"
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
